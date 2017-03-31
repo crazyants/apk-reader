0aeafe339560c8d90a522818f0af294c4931d55c
Soar、毅 <Soar360@live.com>
2017-03-31 18:50:53 +0800

    实现 Run 工具。

--- src/ApkReader.Run/Program.cs
+++ src/ApkReader.Run/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using Ionic.Zip;
 
 namespace ApkReader.Run
 {
@@ -7,37 +9,80 @@ namespace ApkReader.Run
     {
         private static void Main(string[] args)
         {
-            var dir = @"";
-            var apkReader = new Iteedee.ApkReader.ApkReader();
-            var info = apkReader.ExtractInfo(File.ReadAllBytes(dir + "AndroidManifest.xml"),
-                File.ReadAllBytes(dir + "resources.arsc"));
-            Console.WriteLine($"Package Name: {info.PackageName}");
-            Console.WriteLine($"Version Name: {info.VersionName}");
-            Console.WriteLine($"Version Code: {info.VersionCode}");
-
-            Console.WriteLine($"App Has Icon: {info.HasIcon}");
-            if (info.IconFileName.Count > 0)
+            if (args.Any())
             {
-                Console.WriteLine($"App Icon: {info.IconFileName[0]}");
-            }
-            Console.WriteLine($"Min SDK Version: {info.MinSdkVersion}");
-            Console.WriteLine($"Target SDK Version: {info.TargetSdkVersion}");
+                var file = args[0];
+                if (File.Exists(file))
+                {
+                    byte[] dataAndroidManifest = null;
+                    byte[] dataResources = null;
+                    using (var zip = ZipFile.Read(file))
+                    {
+                        foreach (var item in zip)
+                        {
+                            var name = item.FileName.ToLower();
+                            switch (name)
+                            {
+                                case "androidmanifest.xml":
+                                    using (var s = item.OpenReader())
+                                    {
+                                        using (var ms = new MemoryStream())
+                                        {
+                                            s.CopyTo(ms);
+                                            dataAndroidManifest = ms.ToArray();
+                                        }
+                                    }
+                                    break;
+                                case "resources.arsc":
+                                    using (var s = item.OpenReader())
+                                    {
+                                        using (var ms = new MemoryStream())
+                                        {
+                                            s.CopyTo(ms);
+                                            dataResources = ms.ToArray();
+                                        }
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                    if (dataAndroidManifest != null && dataResources != null)
+                    {
+                        var apkReader = new Iteedee.ApkReader.ApkReader();
+                        var info = apkReader.ExtractInfo(dataAndroidManifest, dataResources);
+                        Console.WriteLine($"Package Name: {info.PackageName}");
+                        Console.WriteLine($"Version Name: {info.VersionName}");
+                        Console.WriteLine($"Version Code: {info.VersionCode}");
 
-            if (info.Permissions != null && info.Permissions.Count > 0)
-            {
-                Console.WriteLine("Permissions:");
-                info.Permissions.ForEach(f => { Console.WriteLine($"   {f}"); });
-            }
-            else
-            {
-                Console.WriteLine("No Permissions Found");
-            }
+                        Console.WriteLine($"App Has Icon: {info.HasIcon}");
+                        if (info.IconFileName.Count > 0)
+                        {
+                            Console.WriteLine($"App Icon: {info.IconFileName[0]}");
+                        }
+                        Console.WriteLine($"Min SDK Version: {info.MinSdkVersion}");
+                        Console.WriteLine($"Target SDK Version: {info.TargetSdkVersion}");
 
-            Console.WriteLine($"Supports Any Density: {info.SupportAnyDensity}");
-            Console.WriteLine($"Supports Large Screens: {info.SupportLargeScreens}");
-            Console.WriteLine($"Supports Normal Screens: {info.SupportNormalScreens}");
-            Console.WriteLine($"Supports Small Screens: {info.SupportSmallScreens}");
-            Console.ReadKey();
+                        if (info.Permissions != null && info.Permissions.Count > 0)
+                        {
+                            Console.WriteLine("Permissions:");
+                            info.Permissions.ForEach(f => { Console.WriteLine($"   {f}"); });
+                        }
+                        else
+                        {
+                            Console.WriteLine("No Permissions Found");
+                        }
+
+                        Console.WriteLine($"Supports Any Density: {info.SupportAnyDensity}");
+                        Console.WriteLine($"Supports Large Screens: {info.SupportLargeScreens}");
+                        Console.WriteLine($"Supports Normal Screens: {info.SupportNormalScreens}");
+                        Console.WriteLine($"Supports Small Screens: {info.SupportSmallScreens}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File not found!");
+                }
+            }
         }
     }
 }
